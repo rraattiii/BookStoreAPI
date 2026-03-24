08a1187aeee307cf30e075301c7b8294ad843431
rraattiii <rati.rati2004@gmail.com>
2026-03-24 21:59:15 +0400

    Added .gitignore

--- obj/Debug/net10.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166c53e10d8b975e6cad6dd8becfaf5aac1b2e18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7a3ac77e2c6fc8aef52f4d6563540036b789d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
